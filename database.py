8121a9d6af6a248e384670665b9ff984213c295b
withsj <jadamsandeep2000@gmail.com>
2020-04-21 14:50:57 +0530

    Signup class in database

--- database.py
+++ database.py
@@ -10,6 +10,17 @@ def sql_command(command):
     except sqlite3.Error as err:
         print(f"SQL Command Error {err}")
 
+def insert_command(command,values):
+    """Insert command , values should be iterable """
+    try:
+        conn = sqlite3.connect("Database//database.db")
+        cur = conn.cursor()
+        cur.execute(command,values)
+        conn.close()
+    except sqlite3.Error as err:
+        print(f"SQL Insert Command Error {err}")
+
+
 
 class SignUP():
 
@@ -20,8 +31,8 @@ class SignUP():
         self.Username = Username
         self.Password = Password
         
-        sql_command("""
-            INSERT INTO Accounts()
-        """)
-
+        insert_command("""
+            INSERT INTO Accounts (Email,Fullname,Username,Password)
+            VALUES (?,?,?,?)
+        """,(self.Email,self.Fullname,self.Username,self.Password))
 
