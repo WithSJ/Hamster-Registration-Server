4fe917e8cda36cd8a71794688ef7f4ee7a752223
withsj <jadamsandeep2000@gmail.com>
2020-04-22 11:04:33 +0530

    database.py update

--- database.py
+++ database.py
@@ -1,30 +1,33 @@
 import sqlite3
 
-def sql_command(command):
-    """connect to database """
-    try:
-        conn = sqlite3.connect("Database//database.db")
-        cur = conn.cursor()
-        cur.execute(command)
-        conn.close()
-    except sqlite3.Error as err:
-        print(f"SQL Command Error {err}")
+class Hamster_Database:
 
-def insert_command(command,values):
-    """Insert command , values should be iterable """
-    try:
-        conn = sqlite3.connect("Database//database.db")
-        cur = conn.cursor()
-        cur.execute(command,values)
-        print(command,values)
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as err:
-        print(f"SQL Insert Command Error {err}")
+    @classmethod
+    def sql_command(cls,command):
+        """connect to database """
+        try:
+            conn = sqlite3.connect("Database//database.db")
+            cur = conn.cursor()
+            cur.execute(command)
+            conn.close()
+        except sqlite3.Error as err:
+            print(f"SQL Command Error {err}")
+    
+    @classmethod
+    def insert_command(cls,command,values):
+        """Insert command , values should be iterable """
+        try:
+            conn = sqlite3.connect("Database//database.db")
+            cur = conn.cursor()
+            cur.execute(command,values)
+            print(command,values)
+            conn.commit()
+            conn.close()
+        except sqlite3.Error as err:
+            print(f"SQL Insert Command Error {err}")
 
 
-
-class SignUP():
+class Signup():
 
     def __init__(self,Email,Fullname,Username,Password):
 
@@ -33,6 +36,11 @@ class SignUP():
         self.Username = Username
         self.Password = Password
         
-        insert_command("""INSERT INTO Accounts VALUES(:Email,:Fullname,:Username,:Password)""",
+        Hamster_Database.insert_command(
+        """INSERT INTO Accounts VALUES(:Email,:Fullname,:Username,:Password)""",
         {"Email":self.Email,"Fullname":self.Username,"Username":self.Username,"Password":self.Password})
+
+
+# class Login():
+#     def __init__(self,Username,Password):
 
